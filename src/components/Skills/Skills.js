506d0fac805b4712cc8f8448556589da69690710
silviaplabon <silviaplabon@gmail.com>
2021-05-08 00:57:34 +0600

    silvia

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faDatabase } from "@fortawesome/free-solid-svg-icons";
@@ -13,36 +13,112 @@ import webdevelopment from '../../images/web1.png';
 import ccc from '../../images/c++.png';
 
 const Skills = () => {
+ useEffect(()=>{
+  document.getElementById('textShowHTML5').style.display="none";
+  document.getElementById('HTML5').addEventListener('mouseenter',function(){
+    document.getElementById('textShowHTML5').style.display="block"; 
+  })
+  document.getElementById('HTML5').addEventListener('mouseleave',function(){
+    document.getElementById('textShowHTML5').style.display="none";
+  })
+  //C++
+  document.getElementById('textShowC++').style.display="none";
+  document.getElementById('C++').addEventListener('mouseenter',function(){
+    document.getElementById('textShowC++').style.display="block"; 
+  })
+  document.getElementById('C++').addEventListener('mouseleave',function(){
+    document.getElementById('textShowC++').style.display="none";
+  })
+  //C
+  document.getElementById('textShowC').style.display="none";
+  document.getElementById('C').addEventListener('mouseenter',function(){
+    document.getElementById('textShowC').style.display="block"; 
+  })
+  document.getElementById('C').addEventListener('mouseleave',function(){
+    document.getElementById('textShowC').style.display="none";
+  })
+},[])
+ 
   return (
     <div className="container">
       <h4 className="text-white text-center mb-4 mt-5">Skills</h4>
       <div className="row ">
-        <div className="col-md-7 imgSkillStyle">
-            <img src={webdevelopment} className="  w-100 h-100"></img>
+        <div className="col-md-4 imgSkillStyle">
+          <img src={webdevelopment} className="  w-100 h-100"></img>
         </div>
-        <div className="col-md-5 ">
-          <h5 className="text-white">Full Stack Deployment</h5><br/>
-          <div className="d-flex flex-wrap">
-          <FontAwesomeIcon icon={faHtml5} className=" iconSkill me-2 mt-2" style={{ color: '#e34c26' }} />
-          <FontAwesomeIcon icon={faCss3} style={{ color: '#264de4' }} className="me-2 mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faBootstrap} style={{ color: '#563d7c' }} className="me-2 mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faJs} style={{ color: '#F0DB4F' }} className="me-2  mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faReact} style={{ color: '#61DBFB' }} className="me-2 mt-2 iconSkill" />
+        <div className="col-md-8 ">
+          <h5 className="text-white">Full Stack Deployment</h5><br />
+          <div className="row row-cols-5">
+            <div className="cards col mt-2 ms-2 d-flex justify-content-center align-items-center" id="HTML5">
+              <FontAwesomeIcon icon={faHtml5} className=" iconSkill  w-100" style={{ color: '#e34c26' }} />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
 
-          <FontAwesomeIcon icon={faJsfiddle} style={{ color: '#68a063' }} className="me-2  mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faNodeJs} style={{ color: '#3c873a' }} className="me-2 mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faDatabase} style={{ color: '#4DB33D' }} className="me-2 mt-2 iconSkill" />
+              <FontAwesomeIcon icon={faCss3} style={{ color: '#264de4' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
 
-          <FontAwesomeIcon icon={faGit} style={{ color: '#f34f29' }} className="me-2 mt-2 iconSkill" />
-          <FontAwesomeIcon icon={faGithub} style={{ color: '#945c54' }} className="me-2  mt-2 iconSkill" />
+            </div>
+            <div className="cards col ms-2 mt-2  d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faBootstrap} style={{ color: '#563d7c' }} className="iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faJs} style={{ color: '#F0DB4F' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faReact} style={{ color: '#61DBFB' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faJsfiddle} style={{ color: '#68a063' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faNodeJs} style={{ color: '#3c873a' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faDatabase} style={{ color: '#4DB33D' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2  d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faGit} style={{ color: '#f34f29' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+              <FontAwesomeIcon icon={faGithub} style={{ color: '#945c54' }} className=" iconSkill" />
+              <h6 className="text-center text-white" id="textShowHTML5">HTML5</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center" id="Firebase">
+              <img src={firebaselogo} style={{ color: '#264de4' }} className="image  iconSkill" ></img>
+              <h6 className="text-center text-white" id="textShowFirebase">HTML5</h6>
+            </div>
+
+
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center" id="Netlify">
+
+              <img src={netlifylogo} style={{ backgroundColor: '#264de4' }} className="image  iconSkill" id="Netlify" />
+              <h6 className="text-center text-white" id="textShowNetlify">Netlify</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+
+              <img src={herokuimg} style={{ color: '#264de4' }} className="image  iconSkill " id="Heroku"/>
+              <h6 className="text-center text-white" id="textShowHeroku">Heroku</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center">
+
+              <img src={c} style={{ color: '#264de4' }} className="image iconSkill" id="C" />
+              <h6 className="text-center text-white" id="textShowC">C</h6>
+            </div>
+            <div className="cards col ms-2 mt-2 d-flex justify-content-center align-items-center" id="C++">
+
+              <img src={ccc} style={{ color: '#264de4' }} className="image  iconSkill" />
+              <h6 className="text-center text-white" id="textShowC++">C++</h6>
+            </div>
+
           </div>
-         
-          <img src={firebaselogo} style={{ color: '#264de4' }} className="image me-2 mt-2 iconSkill" ></img>
-          <img src={netlifylogo} style={{ backgroundColor: '#264de4' }} className="image mt-2 me-2 iconSkill" />
-          <img src={herokuimg} style={{ color: '#264de4' }} className="image me-2 iconSkill mt-2" />
-          <img src={c} style={{ color: '#264de4' }} className="image me-2 iconSkill mt-2" />
-          <img src={ccc} style={{ color: '#264de4' }} className="image me-2 iconSkill mt-2" />
-
 
         </div>
 
